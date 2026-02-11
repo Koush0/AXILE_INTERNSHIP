b3f83fab5227a02dd4200a06624479032e635432
Koush0 <koushikyalagi915@gmail.com>
2026-02-11 22:27:03 +0530

    Create student_dictionary.py

---
+++ Week_02/Day_02/student_dictionary.py
@@ -0,0 +1,10 @@
+student = {
+    "name": "Rahul",
+    "age": 20,
+    "course": "B.E",
+    "city": "Chennai"
+}
+
+print("Student Details:")
+for key, value in student.items():
+    print(key, ":", value)
